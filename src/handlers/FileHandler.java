2c241cac4cadddff4267e9f49303a1715c8f78db
Heroist Hero <firestinger_1@hotmail.com>
2016-11-09 13:30:42 -0700

    Refactoring of FileHandler
    -implemented read()
    -changed bufferedReader to fileReader
    -increased javadoc coverage

--- src/handlers/FileHandler.java
+++ src/handlers/FileHandler.java
@@ -1,44 +1,63 @@
 package handlers;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Files;
 
+/**
+ * A handler for the storage layer. This one stores in a file in the current working directory of the program.
+ */
 public class FileHandler {
-	
-	private BufferedWriter bufferedWriter;
-	
-	
+
+	private File file;
+	private FileWriter fileWriter;
+	private static String newLine = System.getProperty("line.separator");
+
+	/**
+	 * @param filename name of the file to be added. this does not include the entire path which is always the current working directory.
+	 *                 If the named file does not exist, it will be created
+	 * @throws IOException if the file could not be created or accessed
+	 */
 	public FileHandler(String filename) throws IOException{
 		//create the file handle
-		File file = new File(System.getProperty("user.dir") + File.separator + filename);
-		
-		//read all contents of the file
-		String temp = "";
-		if (file.exists()){
-			temp = new String(Files.readAllBytes(file.toPath()));
-		}
-		
+		file = new File(System.getProperty("user.dir") + File.separator + filename);
+
 		//create the writer object which will be used in all future write operations
-		bufferedWriter = Files.newBufferedWriter(file.toPath());
-		
-		//write the existing contents back to the file
-		//TODO find a more efficient way
-		bufferedWriter.newLine();
-		bufferedWriter.write(temp);
-		bufferedWriter.flush();
+		fileWriter = new FileWriter(file);
 	}
-	
-	public void append(String line) throws IOException{
-		bufferedWriter.newLine();
-		bufferedWriter.write(line);
-		bufferedWriter.flush();
+
+	/**
+	 * appends a string to the end of a file on it's own line
+	 * @param data the line to add
+	 * @throws IOException if the file could not be written to. likely means the file was deleted
+	 */
+	public void append(String data) throws IOException{
+		fileWriter.append(data);
+		fileWriter.append(newLine);
+		fileWriter.flush();
 	}
-	
+
+	/**
+	 * returns the contents of the file as a string array
+	 * @return an array of all the lines in the file, or an empty array if the file could not be read or is empty
+	 */
+	public String[] read(){
+		try {
+			return new String(Files.readAllBytes(file.toPath())).split(newLine);
+		} catch (IOException e) {
+			//implies file does not exist
+			//e.printStackTrace();
+
+			//return empty array
+			return new String[0];
+		}
+	}
+
+	/**
+	 * close the streams used to read and write
+	 */
 	public void close(){
 		try {
-			bufferedWriter.close();
+			fileWriter.close();
 		} catch (IOException e) {
 			// obviously, if closing fails, it is already closed
 			//e.printStackTrace();
