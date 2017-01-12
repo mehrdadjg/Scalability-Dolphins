f65500c0c8c5e06cc3fcc1c80252db6112b686c6
Heroist Hero <firestinger_1@hotmail.com>
2017-01-12 11:33:29 -0700

    Removed code IDE dependency
    -changed usages of gridlayout to gridbaglayout to improve portability
    -increased default window width from 400 to 410

--- src/IO/ClientView.java
+++ src/IO/ClientView.java
@@ -5,9 +5,8 @@ import util.DocumentUpdate;
 import util.Resources;
 
 import javax.swing.*;
-import javax.swing.event.ChangeEvent;
-import javax.swing.event.ChangeListener;
 import javax.swing.text.BadLocationException;
+import java.awt.*;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.util.Observable;
@@ -17,7 +16,7 @@ import java.util.Observer;
  * INCOMPLETE
  * Created by Arnold on 2016-12-27.
  */
-public class ClientView extends JFrame implements Observer{
+public class ClientView extends JFrame implements Observer {
 
     private JPanel jPanel;
     private JTextArea editingTextArea;
@@ -30,7 +29,7 @@ public class ClientView extends JFrame implements Observer{
     private JButton resetPortButton;
     private JButton connectButton;
     private JPanel ListTab;
-    private JList docsList;
+    private JList<String> docsList;
     private JButton refreshButton;
     private JButton openButton;
     private final DefaultListModel<String> docsModel = new DefaultListModel<>();
@@ -41,9 +40,9 @@ public class ClientView extends JFrame implements Observer{
         //Init the window
         super(windowName);
         setContentPane(jPanel);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         pack();
-        setSize(400, 400);
+        setSize(410, 400);
 
         //Provide the initial values for the address and port
         addressTextField.setText("127.0.0.1");
@@ -59,11 +58,9 @@ public class ClientView extends JFrame implements Observer{
         refreshButton.addActionListener(e -> Client.sender.getList());
         openButton.addActionListener(e -> Client.sender.open(docsModel.elementAt(docsList.getSelectedIndex())));
 
-        //TODO enable once handlers are in place to detect successful connection to proxy
         //disable tabs that require a connection to the proxy to work properly
         jTabbedPane.setEnabledAt(jTabbedPane.indexOfComponent(ListTab), false);
         jTabbedPane.setEnabledAt(jTabbedPane.indexOfComponent(EditingTab), false);
-
 
 
         //disable the highlighter as the communications protocol does not account for its operation
@@ -74,7 +71,7 @@ public class ClientView extends JFrame implements Observer{
             @Override
             public void keyPressed(KeyEvent e) {
                 //the backspace key event is executed on keyPressed event as well as keyTyped. We override it here so that it doesn't trigger two separate document updates
-                if (e.getKeyChar() != KeyEvent.CHAR_UNDEFINED){
+                if (e.getKeyChar() != KeyEvent.CHAR_UNDEFINED) {
                     //consume the key event to prevent it from being executed now as well as later when the proxy confirms it
                     e.consume();
                 }
@@ -83,12 +80,12 @@ public class ClientView extends JFrame implements Observer{
             @Override
             public void keyTyped(KeyEvent e) {
                 int index = editingTextArea.getCaretPosition();
-                if (e.getKeyChar() != KeyEvent.CHAR_UNDEFINED){
+                if (e.getKeyChar() != KeyEvent.CHAR_UNDEFINED) {
                     //consume the key event to prevent it from being executed now as well as later when the proxy confirms it
                     e.consume();
 
                     //remove operations will delete the character after the caret, but a backspace deletes the character before. we decrement the index to set the removal in the correct position
-                    if (e.getKeyChar() == KeyEvent.VK_BACK_SPACE){
+                    if (e.getKeyChar() == KeyEvent.VK_BACK_SPACE) {
                         index--;
                     }
                     //convert delete to a backspace event without decrementing the index as it is already in the correct location
@@ -102,14 +99,14 @@ public class ClientView extends JFrame implements Observer{
 
             @Override
             public void keyReleased(KeyEvent e) {
-                if (e.getKeyChar() != KeyEvent.CHAR_UNDEFINED){
+                if (e.getKeyChar() != KeyEvent.CHAR_UNDEFINED) {
                     e.consume();
                 }
             }
         });
         jTabbedPane.addChangeListener(e -> {
-            if (jTabbedPane.getSelectedIndex() == jTabbedPane.indexOfComponent(ListTab)){
-                if (listUpdated == false){
+            if (jTabbedPane.getSelectedIndex() == jTabbedPane.indexOfComponent(ListTab)) {
+                if (!listUpdated) {
                     Client.sender.getList();
                     listUpdated = true;
                 }
@@ -122,52 +119,56 @@ public class ClientView extends JFrame implements Observer{
     /**
      * Show the GUI
      */
-    public void create(){
+    public void create() {
         setVisible(true);
     }
 
     /**
      * A getter for the data inserted by the user for the address and port fields
+     *
      * @return A string of address and port separated by a colon
      */
-    public String getAddress(){
+    public String getAddress() {
         return addressTextField.getText() + ":" + portTextField.getText();
     }
 
     /**
      * A setter for the list which shows the documents available
-     * @param list
+     *
+     * @param list A String array of available documents
      */
-    public void setList(String[] list) {
+    private void setList(String[] list) {
         docsModel.removeAllElements();
-        for(String s : list){
+        for (String s : list) {
             docsModel.addElement(s);
         }
     }
 
     /**
      * Report a change that should be made to the document.
+     *
      * @param element The character that was added. This may be a backspace to represent a deletion
-     * @param index The position that the character should be added to or removed from
+     * @param index   The position that the character should be added to or removed from
      */
-    private void documentChanged(char element, int index){
+    private void documentChanged(char element, int index) {
         DocumentUpdate documentUpdate = new DocumentUpdate(element, index, Client.getAndIncreaseTransformationNumber());
         Client.sender.sendUpdate(documentUpdate);
     }
 
     /**
      * Make a change to the document shown in the editor text area
+     *
      * @param documentUpdate The documentUpdate representing the change to be made to a document state
      */
-    public void addAt(DocumentUpdate documentUpdate){
+    private void addAt(DocumentUpdate documentUpdate) {
         String element = documentUpdate.getString();
         int actualPos = documentUpdate.getPosition(DocumentUpdate.PositionType.Actual);
         int intendedPos = documentUpdate.getPosition(DocumentUpdate.PositionType.Intended);
         int index = (actualPos < 0) ? intendedPos : actualPos;
 
-        if (element.charAt(0) != DocumentUpdate.BACKSPACE){
-            editingTextArea.insert(element,index);
-        } else if (element.charAt(0) == DocumentUpdate.BACKSPACE){
+        if (element.charAt(0) != DocumentUpdate.BACKSPACE) {
+            editingTextArea.insert(element, index);
+        } else if (element.charAt(0) == DocumentUpdate.BACKSPACE) {
             try {
                 //jTextArea does not support character removal. We use the remove method of the underlying document class
                 editingTextArea.getDocument().remove(index, 1);
@@ -177,19 +178,155 @@ public class ClientView extends JFrame implements Observer{
         }
     }
 
-    public void connected(boolean isConnected){
+    private void connected(boolean isConnected) {
         jTabbedPane.setEnabledAt(jTabbedPane.indexOfComponent(ListTab), isConnected);
         jTabbedPane.setEnabledAt(jTabbedPane.indexOfComponent(EditingTab), isConnected);
     }
 
     @Override
-    public void update(Observable observable, Object arg){
-        if (arg instanceof DocumentUpdate){
+    public void update(Observable observable, Object arg) {
+        if (arg instanceof DocumentUpdate) {
             addAt((DocumentUpdate) arg);
-        } else if (arg instanceof String[]){
+        } else if (arg instanceof String[]) {
             setList((String[]) arg);
         } else {
             connected(Client.isConnected);
         }
+    }
+
+    {
+// GUI initializer generated by IntelliJ IDEA GUI Designer
+// >>> IMPORTANT!! <<<
+// DO NOT EDIT OR ADD ANY CODE HERE!
+        $$$setupUI$$$();
+    }
+
+    /**
+     * Method generated by IntelliJ IDEA GUI Designer
+     * >>> IMPORTANT!! <<<
+     * DO NOT edit this method OR call it in your code!
+     *
+     * @noinspection ALL
+     */
+    private void $$$setupUI$$$() {
+        jPanel = new JPanel();
+        jPanel.setLayout(new BorderLayout(0, 0));
+        jTabbedPane = new JTabbedPane();
+        jPanel.add(jTabbedPane, BorderLayout.SOUTH);
+        ConnectionTab = new JPanel();
+        ConnectionTab.setLayout(new GridBagLayout());
+        jTabbedPane.addTab("Connection", ConnectionTab);
+        addressTextField = new JTextField();
+        addressTextField.setColumns(10);
+        GridBagConstraints gbc;
+        gbc = new GridBagConstraints();
+        gbc.gridx = 2;
+        gbc.gridy = 0;
+        gbc.anchor = GridBagConstraints.WEST;
+        ConnectionTab.add(addressTextField, gbc);
+        final JPanel spacer1 = new JPanel();
+        gbc = new GridBagConstraints();
+        gbc.gridx = 3;
+        gbc.gridy = 0;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        ConnectionTab.add(spacer1, gbc);
+        portTextField = new JTextField();
+        portTextField.setColumns(5);
+        gbc = new GridBagConstraints();
+        gbc.gridx = 2;
+        gbc.gridy = 1;
+        gbc.anchor = GridBagConstraints.WEST;
+        ConnectionTab.add(portTextField, gbc);
+        final JLabel label1 = new JLabel();
+        label1.setText("Proxy Address");
+        gbc = new GridBagConstraints();
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.anchor = GridBagConstraints.NORTHWEST;
+        ConnectionTab.add(label1, gbc);
+        final JLabel label2 = new JLabel();
+        label2.setText("Proxy Port");
+        gbc = new GridBagConstraints();
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+        gbc.anchor = GridBagConstraints.NORTHWEST;
+        ConnectionTab.add(label2, gbc);
+        resetAddressButton = new JButton();
+        resetAddressButton.setText("Reset");
+        gbc = new GridBagConstraints();
+        gbc.gridx = 4;
+        gbc.gridy = 0;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        ConnectionTab.add(resetAddressButton, gbc);
+        resetPortButton = new JButton();
+        resetPortButton.setText("Reset");
+        gbc = new GridBagConstraints();
+        gbc.gridx = 4;
+        gbc.gridy = 1;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        ConnectionTab.add(resetPortButton, gbc);
+        final JPanel spacer2 = new JPanel();
+        gbc = new GridBagConstraints();
+        gbc.gridx = 1;
+        gbc.gridy = 0;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        ConnectionTab.add(spacer2, gbc);
+        final JPanel spacer3 = new JPanel();
+        gbc = new GridBagConstraints();
+        gbc.gridx = 5;
+        gbc.gridy = 2;
+        gbc.fill = GridBagConstraints.VERTICAL;
+        gbc.ipady = 200;
+        ConnectionTab.add(spacer3, gbc);
+        connectButton = new JButton();
+        connectButton.setText("Connect");
+        connectButton.setVerticalAlignment(0);
+        gbc = new GridBagConstraints();
+        gbc.gridx = 5;
+        gbc.gridy = 3;
+        gbc.anchor = GridBagConstraints.SOUTHEAST;
+        ConnectionTab.add(connectButton, gbc);
+        ListTab = new JPanel();
+        ListTab.setLayout(new BorderLayout(0, 0));
+        jTabbedPane.addTab("List", ListTab);
+        docsList = new JList();
+        ListTab.add(docsList, BorderLayout.CENTER);
+        final JPanel panel1 = new JPanel();
+        panel1.setLayout(new GridBagLayout());
+        ListTab.add(panel1, BorderLayout.SOUTH);
+        refreshButton = new JButton();
+        refreshButton.setText("Refresh");
+        gbc = new GridBagConstraints();
+        gbc.gridx = 1;
+        gbc.gridy = 0;
+        gbc.weightx = 1.0;
+        gbc.weighty = 1.0;
+        gbc.anchor = GridBagConstraints.EAST;
+        panel1.add(refreshButton, gbc);
+        openButton = new JButton();
+        openButton.setText("Open");
+        gbc = new GridBagConstraints();
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.weightx = 1.0;
+        gbc.weighty = 1.0;
+        gbc.anchor = GridBagConstraints.WEST;
+        panel1.add(openButton, gbc);
+        EditingTab = new JPanel();
+        EditingTab.setLayout(new BorderLayout(0, 0));
+        jTabbedPane.addTab("Editing", EditingTab);
+        final JScrollPane scrollPane1 = new JScrollPane();
+        EditingTab.add(scrollPane1, BorderLayout.CENTER);
+        editingTextArea = new JTextArea();
+        editingTextArea.setRows(20);
+        editingTextArea.setText("");
+        scrollPane1.setViewportView(editingTextArea);
+    }
+
+    /**
+     * @noinspection ALL
+     */
+    public JComponent $$$getRootComponent$$$() {
+        return jPanel;
     }
 }
